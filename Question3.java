3ca9280e87c58c5263eb467ebd37c2a5a7a07e09
Tommy Quinlan <quinlant3@winthrop.edu>
2026-02-25 17:54:52 -0500

    Uploaded Question3

---
+++ Question3.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+/*
+ * Complexity Work:
+ * 
+ * n = size of array.
+ *
+ * Equation:
+ * T(n) = T(n-1) + c
+ *
+ * Complexity = O(n)
+ */
+
+public class Q3_MaxRecursive {
+
+    public static int max(int[] A, int n) {
+        if (n == 1) {
+            return A[0];
+        }
+
+        int maxRest = max(A, n - 1);
+
+        return (A[n - 1] > maxRest) ? A[n - 1] : maxRest;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter number of elements: ");
+        int size = input.nextInt();
+
+        int[] A = new int[size];
+
+        System.out.println("Enter integers:");
+        for (int i = 0; i < size; i++) {
+            A[i] = input.nextInt();
+        }
+
+        int maximum = max(A, size);
+
+        System.out.println("Maximum value = " + maximum);
+
+        /*
+         * Test Cases:
+         * 1. [1,2,3,4,5] - 5
+         * 2. [-7,-1,-4] - -1
+         * 3. [100] - 100
+         */
+    }
+}
